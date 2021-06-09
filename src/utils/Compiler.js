bc8b15fa24b267de591b3b25bd5ad9e2133cff0e
wipaaa <ini.wipaaa@gmail.com>
2021-06-09 12:43:48 +0800

    Add Replacer to connect to Compiler

--- src/utils/Compiler.js
+++ src/utils/Compiler.js
@@ -1,3 +1,5 @@
+const Replacer = require('./Replacer');
+
 module.exports = class Compiler {
   constructor() {
     this.input = {
@@ -6,7 +8,69 @@ module.exports = class Compiler {
       settings: { outputSelection: { ['*']: { ['*']: ['*'] } } },
     };
     this.regex = /__REPLACE_(\w+)__/gi;
-    this.replacements = {};
+    this.replacer = new Replacer(this.regex);
     this.sources = [];
   }
+
+  compile = (source = null) => {
+    if (source && Array.isArray(source)) {
+      // while source is provided and the source type is an
+      // array we'll assume that u want to compile the
+      // source with '_compileFromSources' method
+      this.setSources(source).#_compileFromSources();
+      return null;
+    }
+
+    if (!source) {
+      // while source isn't provided then we'll assume
+      // that u want to compile the source from the
+      // sources object by calling the 'setSources'
+      this.#_compileFromSources();
+      return null;
+    }
+
+    return this.#_compileFromSource(source);
+  };
+
+  setRules = (rules) => {
+    this.replacer.setRules(rules);
+    return this;
+  };
+
+  setSources = (sources) => {
+    if (!Array.isArray(sources)) {
+      const message = `Sources is expected to be array instead of ${typeof sources}`;
+      throw new TypeError(message);
+    }
+
+    if (!sources.length) {
+      // while the given sources is empty than we'll
+      // just return this object to continue the
+      // flow so the program won't crash
+      return this;
+    }
+
+    this.sources = sources;
+    return this;
+  };
+
+  #_compileFromSource = (source) => {
+    const compiled = this.replacer.replace(source);
+    return compiled;
+  };
+
+  #_compileFromSources = () => {
+    if (!this.sources.length) {
+      return null;
+    }
+
+    this.sources.forEach((source, id) => {
+      this.sources[id] = {
+        ...source,
+        content: this.#_compileFromSource(source.content),
+      };
+    });
+
+    return null;
+  };
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 const Compiler = require('./Compiler');
+const Replacer = require('./Replacer');
 const Resolver = require('./Resolver');
 
 module.exports = {
   compiler: new Compiler(),
+  replacer: new Replacer(),
   resolver: new Resolver(),
 };
