64a51584fa984c187621689f0c50c41e9636e490
wipaaa <ini.wipaaa@gmail.com>
2021-06-10 20:37:03 +0800

    Fixing the resolver test

--- src/utils/__tests__/utils-resolver.spec.js
+++ src/utils/__tests__/utils-resolver.spec.js
@@ -1,109 +1,97 @@
 import Resolver from '../Resolver';
 
-const defaults = {
-  charset: 'utf-8',
-  data: {
-    name: null,
-    content: null,
-    path: null,
-  },
-  directory: '../../stub',
-  extension: {
-    from: '.txt',
-    to: '.sol',
-  },
-};
-
-jest.mock('../Resolver');
-
-describe('Resolver utility', () => {
+describe('Resolver utility test', () => {
   let resolver = null;
 
   beforeAll(() => {
     resolver = new Resolver();
+
+    jest.spyOn(resolver, 'resolve');
+    jest.spyOn(resolver, 'setFrom');
   });
 
   describe('when instantiated', () => {
-    it('should be an a truthy', () => {
-      expect(resolver).not.toBeFalsy();
-      expect(resolver).not.toBeNull();
-      expect(resolver).not.toBeUndefined();
-    });
-
-    it('should be a valid instance', () => {
+    it('should be an instance of resolver class', () => {
+      expect(resolver).toBeTruthy();
       expect(resolver).toBeInstanceOf(Resolver);
     });
 
-    it('should contain a valid property', () => {
+    it('should have property sources', () => {
       expect(resolver).toHaveProperty('sources', []);
-      expect(resolver).toHaveProperty('defaults', defaults);
+    });
+
+    it('should have property defaults and match defaults property snapshot', () => {
+      expect(resolver).toHaveProperty('defaults');
+      expect(resolver.defaults).toMatchSnapshot();
     });
   });
 
   describe('when resolve method called', () => {
-    afterEach(() => {
-      expect(resolver.resolve).toHaveReturned();
-    });
-
     beforeEach(() => {
       resolver.resolve.mockClear();
     });
 
     it('should not throw any error', () => {
       expect(() => resolver.resolve()).not.toThrow();
+      expect(resolver.resolve).toHaveBeenCalled();
     });
 
-    it('should return the sources object', () => {
-      resolver.setFrom('../stub');
-
-      const result = resolver.resolve();
+    it('should return empty array when no sources provided', () => {
+      const sources = resolver.resolve();
 
       expect(resolver.resolve).toHaveBeenCalled();
       expect(resolver.resolve).toHaveReturned();
-      expect(result).toMatchSnapshot();
-
-      resolver.setFrom.mockClear();
+      expect(sources).toEqual([]);
     });
 
-    it('should reset the sources property', () => {
-      resolver.resolve();
-      expect(resolver.sources).toEqual([]);
+    it('should match the sources snapshot', () => {
+      resolver.sources = [];
+
+      const path = '../stub';
+      const sources = resolver.setFrom(path).resolve();
+
+      expect(resolver.setFrom).toHaveBeenCalledWith(path);
+      expect(resolver.setFrom).toHaveReturnedWith(resolver);
+      expect(resolver.resolve).toHaveBeenCalled();
+      expect(resolver.resolve).toHaveReturned();
+      expect(sources).toMatchSnapshot();
+
+      resolver.setFrom.mockClear();
     });
   });
 
   describe('when setFrom method called', () => {
-    afterEach(() => {
-      expect(resolver.setFrom).toHaveBeenCalled();
-    });
-
     beforeEach(() => {
+      resolver.sources = [];
       resolver.setFrom.mockClear();
     });
 
-    it('should throw an error while invalid path specified', () => {
-      const fakePath = '/fake/path';
-      const errorMessage = `Directory isn't exists at: ${fakePath}.`;
-
+    it('should throw an error while provided with invalid path', () => {
       expect(() => resolver.setFrom('/fake/path')).toThrow();
-      expect(() => resolver.setFrom('/fake/path')).toThrow(errorMessage);
+      expect(resolver.setFrom).toHaveBeenCalled();
     });
 
-    it('should return resolver object while success', () => {
-      const actualPath = '../stub';
-      const result = resolver.setFrom(actualPath);
+    it('should return the resolver object itself', () => {
+      const instance = resolver.setFrom('../stub');
 
-      expect(result).toBeInstanceOf(Resolver);
       expect(resolver.setFrom).toHaveBeenCalled();
-      expect(resolver.setFrom).toHaveBeenCalledWith(actualPath);
-      expect(resolver.setFrom).toHaveReturnedWith(resolver);
+      expect(resolver.setFrom).toHaveReturned();
+      expect(instance).toBeTruthy();
+      expect(instance).toBeInstanceOf(Resolver);
     });
 
-    it('should change the sources property with directory files list', () => {
-      const actualPath = '../stub';
-      const result = resolver.setFrom(actualPath);
+    it('should change the sources property', () => {
+      resolver.setFrom('../stub');
 
       expect(resolver.setFrom).toHaveBeenCalled();
-      expect(resolver.setFrom).toHaveBeenCalledWith(actualPath);
+      expect(resolver.setFrom).toHaveReturned();
+      expect(resolver.sources).not.toEqual([]);
+    });
+
+    it('should match the sources snapshot', () => {
+      resolver.setFrom('../stub');
+
+      expect(resolver.setFrom).toHaveBeenCalled();
       expect(resolver.setFrom).toHaveReturned();
       expect(resolver.sources).toMatchSnapshot();
     });
