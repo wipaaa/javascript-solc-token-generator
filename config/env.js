c3ec8d626f1109267b2321001aa769bd26d2ae5d
wipaaa <ini.wipaaa@gmail.com>
2021-06-10 10:10:28 +0800

    Add version to env file

--- config/env.js
+++ config/env.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
+  SOLC_VERSION: process.env.SOLC_VERSION ?? 'v0.5.12+commit.7709ece9',
   WEB3_PROVIDER_LOCAL: process.env.WEB3_PROVIDER_LOCAL,
   WEB3_PROVIDER_REMOTE: process.env.WEB3_PROVIDER_REMOTE,
   WEB3_ACC_PRIVATE_KEY: process.env.WEB3_ACC_PRIVATE_KEY,
